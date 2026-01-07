d014c7e47672775ed3244f3aa4879545336b1ed3
aurkaxi <abdullahalmuaz15@gmail.com>
2026-01-08 02:53:54 +0600

    refactor: move dap keys to lazy plugin

--- lua/plugins/debugger.lua
+++ lua/plugins/debugger.lua
@@ -25,5 +25,63 @@ return {
 
         -- Python
         require("dap-python").setup("uv")
-    end
+    end,
+    keys = { -- DAP Keymaps
+    {
+        "<leader>db",
+        function()
+            require("dap").toggle_breakpoint()
+        end,
+        desc = "Debug: Toggle Breakpoint"
+    }, {
+        "<leader>dc",
+        function()
+            require("dap").continue()
+        end,
+        desc = "Debug: Continue"
+    }, {
+        "<leader>dr",
+        function()
+            require("dap").terminate()
+        end,
+        desc = "Debug: Restart"
+    }, {
+        "<leader>dq",
+        function()
+            require("dap").terminate()
+        end,
+        desc = "Debug: Terminate"
+    }, {
+        "<leader>dn",
+        function()
+            require("dap").step_over()
+        end,
+        desc = "Debug: Step Over"
+    }, {
+        "<leader>di",
+        function()
+            require("dap").step_into()
+        end,
+        desc = "Debug: Step Into"
+    }, {
+        "<leader>do",
+        function()
+            require("dap").step_out()
+        end,
+        desc = "Debug: Step Out"
+    }, -- DAP UI Keymaps
+    {
+        "<leader>du",
+        function()
+            require("dapui").toggle()
+        end,
+        desc = "Debug: Toggle UI"
+    }, {
+        "<leader>de",
+        function()
+            require("dapui").eval()
+        end,
+        desc = "Debug: Eval",
+        mode = {"n", "v"}
+    }}
 }
