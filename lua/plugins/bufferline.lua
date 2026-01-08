cab33d7b06966e5b7832c1daafe8d12c587f110f
aurkaxi <abdullahalmuaz15@gmail.com>
2026-01-08 19:41:34 +0600

    fix: fix bufferline misconfiguration

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -6,7 +6,7 @@ return {
             -- mode: "buffers" default or "tabs" (only show tabpages)
             mode = "buffers",
             -- style: bufferline.style_preset.default or bufferline.style_preset.minimal
-            style_preset = "default",
+            -- style_preset = "default",
             -- themable: true | false
             themable = true,
             -- numbers: "none" | "ordinal" | "buffer_id" | "both" | function({ ordinal, id, lower, raise }): string,
@@ -33,13 +33,13 @@ return {
             --- Please note some names can/will break the
             --- bufferline so use this at your discretion knowing that it has
             --- some limitations that will *NOT* be fixed.
-            name_formatter = function(buf) -- buf contains:
-                -- name                | str        | the basename of the active file
-                -- path                | str        | the full path of the active file
-                -- bufnr               | int        | the number of the active buffer
-                -- buffers (tabs only) | table(int) | the numbers of the buffers in the tab
-                -- tabnr (tabs only)   | int        | the "handle" of the tab, can be converted to its ordinal number using: `vim.api.nvim_tabpage_get_number(buf.tabnr)`
-            end,
+            -- name_formatter = function(buf) -- buf contains:
+            -- name                | str        | the basename of the active file
+            -- path                | str        | the full path of the active file
+            -- bufnr               | int        | the number of the active buffer
+            -- buffers (tabs only) | table(int) | the numbers of the buffers in the tab
+            -- tabnr (tabs only)   | int        | the "handle" of the tab, can be converted to its ordinal number using: `vim.api.nvim_tabpage_get_number(buf.tabnr)`
+            -- end,
             max_name_length = 18,
             max_prefix_length = 15, -- prefix used when a buffer is de-duplicated
             truncate_names = true,  -- whether or not tab names should be truncated
@@ -50,30 +50,30 @@ return {
             diagnostics_update_in_insert = false, -- only applies to coc
             diagnostics_update_on_event = true,   -- use nvim's diagnostic handler
             -- The diagnostics indicator can be set to nil to keep the buffer name highlight but delete the highlighting
-            diagnostics_indicator = function(count, level, diagnostics_dict, context)
-                return "(" .. count .. ")"
-            end,
+            -- diagnostics_indicator = function(count, level, diagnostics_dict, context)
+            --     return "(" .. count .. ")"
+            -- end,
             --
             -- NOTE: this will be called a lot so don't do any heavy processing here
-            custom_filter = function(buf_number, buf_numbers)
-                -- filter out filetypes you don't want to see
-                -- if vim.bo[buf_number].filetype ~= "<i-dont-want-to-see-this>" then
-                --   return true
-                -- end
-                -- filter out by buffer name
-                -- if vim.fn.bufname(buf_number) ~= "<buffer-name-I-dont-want>" then
-                --     return true
-                -- end
-                -- filter out based on arbitrary rules
-                -- e.g. filter out vim wiki buffer from tabline in your work repo
-                -- if vim.fn.getcwd() == "<work-repo>" and vim.bo[buf_number].filetype ~= "wiki" then
-                --     return true
-                -- end
-                -- filter out by it's index number in list (don't show first buffer)
-                -- if buf_numbers[1] ~= buf_number then
-                --     return true
-                -- end
-            end,
+            -- custom_filter = function(buf_number, buf_numbers)
+            -- filter out filetypes you don't want to see
+            -- if vim.bo[buf_number].filetype ~= "<i-dont-want-to-see-this>" then
+            --   return true
+            -- end
+            -- filter out by buffer name
+            -- if vim.fn.bufname(buf_number) ~= "<buffer-name-I-dont-want>" then
+            --     return true
+            -- end
+            -- filter out based on arbitrary rules
+            -- e.g. filter out vim wiki buffer from tabline in your work repo
+            -- if vim.fn.getcwd() == "<work-repo>" and vim.bo[buf_number].filetype ~= "wiki" then
+            --     return true
+            -- end
+            -- filter out by it's index number in list (don't show first buffer)
+            -- if buf_numbers[1] ~= buf_number then
+            --     return true
+            -- end
+            -- end,
 
             -- offsets (sidebars like nvimtree)
             offsets = {
@@ -87,17 +87,17 @@ return {
 
             -- visual configuration
             color_icons = true, -- whether or not to add the filetype icon highlights
-            get_element_icon = function(element)
-                -- element consists of {filetype: string, path: string, extension: string, directory: string}
-                -- This can be used to change how bufferline fetches the icon
-                -- for an element e.g. a buffer or a tab.
-                -- e.g.
-                local icon, hl = require('nvim-web-devicons').get_icon_by_filetype(element.filetype, { default = false })
-                return icon, hl
-                -- or
-                -- local custom_map = {my_thing_ft: {icon = "my_thing_icon", hl}}
-                -- return custom_map[element.filetype]
-            end,
+            -- get_element_icon = function(element)
+            -- element consists of {filetype: string, path: string, extension: string, directory: string}
+            -- This can be used to change how bufferline fetches the icon
+            -- for an element e.g. a buffer or a tab.
+            -- e.g.
+            -- local icon, hl = require('nvim-web-devicons').get_icon_by_filetype(element.filetype, { default = false })
+            -- return icon, hl
+            -- or
+            -- local custom_map = {my_thing_ft: {icon = "my_thing_icon", hl}}
+            -- return custom_map[element.filetype]
+            -- end,
             show_buffer_icons = true, -- disable filetype icons for buffers
             show_buffer_close_icons = true,
             show_close_icon = true,
