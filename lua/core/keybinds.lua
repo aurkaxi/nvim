b30313444ad76aa1bc01cdf8b74ccf37b6b8e1cf
aurkaxi <abdullahalmuaz15@gmail.com>
2026-03-07 22:54:38 +0600

    feat: add keybind for code formatting

--- lua/core/keybinds.lua
+++ lua/core/keybinds.lua
@@ -1,3 +1,6 @@
 vim.keymap.set(
 	"n", "<leader>e", "<cmd>Lex<CR>", { desc = "toggle explorer" }
 )
+vim.keymap.set(
+	"n", "<leader>cf", "<cmd>lua vim.lsp.buf.format()<CR>", { desc = "Code Format" }
+)
