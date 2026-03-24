2cfba10177a5614873b4e1f3ed4ae81c87a712c0
aurkaxi <abdullahalmuaz15@gmail.com>
2026-03-25 00:32:25 +0600

    feat: add inlay hints
    reduced tabspace to 2

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,60 +1,64 @@
 return {
-	"neovim/nvim-lspconfig",
-	config = function()
-		local lsp_group = vim.api.nvim_create_augroup('UserLspConfig', { clear = true })
+  "neovim/nvim-lspconfig",
+  config = function()
+    local lsp_group = vim.api.nvim_create_augroup('UserLspConfig', { clear = true })
 
-		-- python
-		vim.lsp.config('ruff', {
-			-- init_options = {
-			--     settings = {
-			--         -- Ruff language server settings go here
-			--     }
-			-- }
-		})
-		vim.lsp.enable('ruff')
-		vim.lsp.enable("pyright")
+    -- python
+    vim.lsp.config('ruff', {
+      -- init_options = {
+      --     settings = {
+      --         -- Ruff language server settings go here
+      --     }
+      -- }
+    })
+    vim.lsp.enable('ruff')
+    vim.lsp.enable("pyright")
 
-		vim.api.nvim_create_autocmd("LspAttach", {
-			group = lsp_group,
-			callback = function(args)
-				local client = vim.lsp.get_client_by_id(args.data.client_id)
-				if client and client.name == 'ruff' then
-					-- Disable ruff hover in favor of Pyright
-					client.server_capabilities.hoverProvider = false
-				end
-			end,
-			desc = 'LSP: Disable specific server capabilities'
-		}
-		)
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = lsp_group,
+      callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client and client.name == 'ruff' then
+          -- Disable ruff hover in favor of Pyright
+          client.server_capabilities.hoverProvider = false
+        end
+      end,
+      desc = 'LSP: Disable specific server capabilities'
+    }
+    )
 
 
-		vim.lsp.config('lua_ls', {})
-		vim.lsp.enable('lua_ls')
-		vim.lsp.enable('dartls')
+    vim.lsp.config('lua_ls', {})
+    vim.lsp.enable('lua_ls')
+    vim.lsp.enable('dartls')
 
-		-- misc
-		vim.api.nvim_create_autocmd("LspAttach", {
-			group = lsp_group,
-			callback = function(args)
-				local client = vim.lsp.get_client_by_id(args.data.client_id)
+    -- misc
+    -- format on save
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = lsp_group,
+      callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
 
-				-- Check if the client supports formatting but not the wait-until method
-				if client and
-				    not client:supports_method('textDocument/willSaveWaitUntil') and client:supports_method('textDocument/formatting') then
-					vim.api.nvim_create_autocmd('BufWritePre', {
-						buffer = args.buf,
-						callback = function()
-							vim.lsp.buf.format({
-								bufnr = args.buf,
-								id = client.id,
-								timeout_ms = 3000,
-							})
-						end,
-						desc = 'LSP: Format on save',
-					})
-				end
-			end,
-			desc = 'LSP: Setup format-on-save'
-		})
-	end
+        -- Check if the client supports formatting but not the wait-until method
+        if client and
+            not client:supports_method('textDocument/willSaveWaitUntil') and client:supports_method('textDocument/formatting') then
+          vim.api.nvim_create_autocmd('BufWritePre', {
+            buffer = args.buf,
+            callback = function()
+              vim.lsp.buf.format({
+                bufnr = args.buf,
+                id = client.id,
+                timeout_ms = 3000,
+              })
+            end,
+            desc = 'LSP: Format on save',
+          })
+        end
+      end,
+      desc = 'LSP: Setup format-on-save'
+    })
+
+    -- inlay hints
+    vim.lsp.inlay_hint.enable();
+  end
 }
