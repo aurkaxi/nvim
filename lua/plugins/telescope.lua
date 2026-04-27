09a67c08dc6fde6bd51841b0376e324e59fb5e9d
aurkaxi <abdullahalmuaz15@gmail.com>
2026-04-27 18:16:16 +0600

    fix(telescope): remove version pin
    bleeding edge like everything else hehe

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,17 +1,16 @@
-return {{
+return { {
     'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
-    dependencies = {'nvim-lua/plenary.nvim', {
+    dependencies = { 'nvim-lua/plenary.nvim', {
         'nvim-telescope/telescope-fzf-native.nvim',
         build = 'make'
-    }},
+    } },
     opts = {
         extensions = {
             fzf = {
-                fuzzy = true, -- false will only do exact matching
+                fuzzy = true,                   -- false will only do exact matching
                 override_generic_sorter = true, -- override the generic sorter
-                override_file_sorter = true, -- override the file sorter
-                case_mode = "smart_case" -- or "ignore_case" or "respect_case"
+                override_file_sorter = true,    -- override the file sorter
+                case_mode = "smart_case"        -- or "ignore_case" or "respect_case"
                 -- the default case_mode is "smart_case"
             }
         }
@@ -21,7 +20,7 @@ return {{
         require('telescope').setup(opts)
         require('telescope').load_extension("fzf")
     end,
-    keys = {{
+    keys = { {
         "<leader>ff",
         function()
             require("telescope.builtin").find_files()
@@ -45,5 +44,5 @@ return {{
             require("telescope.builtin").help_tags()
         end,
         desc = "Telescope help tags"
-    }}
-}}
+    } }
+} }
