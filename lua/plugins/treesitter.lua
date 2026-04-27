7b2d7d5ffefccf16e6cb20b3096eb86dc85f8472
aurkaxi <abdullahalmuaz15@gmail.com>
2026-04-27 14:49:27 +0600

    fix(treesitter): migrate to 'main' for nvim 0.12

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,28 +1,34 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    branch = 'master',
+    branch = 'main',
     lazy = false,
     build = ":TSUpdate",
-    main = "nvim-treesitter.configs",
-    opts = {
-        ensure_installed = {"c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline"},
-
-        -- Install parsers synchronously (only applied to `ensure_installed`)
-        sync_install = false,
-
-        -- Automatically install missing parsers when entering buffer
-        auto_install = true,
-
-        -- List of parsers to ignore installing (or "all")
-        ignore_install = { --
-        -- repo doesn't exist anymore
-        "ipkg", -- 
-        -- This version of Tree-sitter can only generate parsers with ABI version 13 - 14, not 15
-        "scfg", "ocamllex", "teal", "unison", "swift", "latex", "mlir"},
-
-        highlight = {
-            enable = true,
-            additional_vim_regex_highlighting = false
+    main = "nvim-treesitter",
+    init = function()
+        local ensureInstalled = {
+            "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "rust"
         }
-    }
+        local alreadyInstalled = require('nvim-treesitter.config').get_installed()
+        local parsersToInstall = vim.iter(ensureInstalled)
+            :filter(function(parser)
+                return not vim.tbl_contains(alreadyInstalled, parser)
+            end)
+            :totable()
+        require('nvim-treesitter').install(parsersToInstall)
+
+        vim.api.nvim_create_autocmd('FileType', {
+            callback = function()
+                -- highlight
+                vim.treesitter.start()
+
+                -- Indentation
+                vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+
+                -- folds
+                vim.wo[0][0].foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+                vim.wo[0][0].foldmethod = 'expr'
+                vim.wo[0][0].foldlevel = 99
+            end,
+        })
+    end
 }
